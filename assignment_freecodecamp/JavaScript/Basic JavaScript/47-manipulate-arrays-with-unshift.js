8587ea6e79c8a3f3511b0d46d199f791da26f60b
ZobaidaHossain <zobaida15-13671@diu.edu.bd>
2022-03-30 20:29:11 +0600

    basic javascript

---
+++ assignment_freecodecamp/JavaScript/Basic JavaScript/47-manipulate-arrays-with-unshift.js
@@ -0,0 +1,7 @@
+// Setup
+const myArray = [["John", 23], ["dog", 3]];
+myArray.shift();
+console.log(myArray);
+myArray.unshift(["Paul",35]);
+console.log(myArray);
+// Only change code below this line
